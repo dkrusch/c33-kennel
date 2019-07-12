f33a8cd35c7e8a370402e00fd74faa458395355b
Daniel Krusch <daniel.krusch@gmail.com>
2019-07-12 11:48:46 -0500

    Added nav bar and owners and stuff

---
+++ src/components/owner/OwnerList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export default class OwnerList extends Component {
+    render() {
+        return (
+            <section className="owners">
+            {
+                this.props.owners.map(owner =>
+                    <div key={owner.id}>
+                        <h4>Name: {owner.name}</h4>
+                        <h5>Phone: {owner.phonenumber}</h5>
+                    </div>
+                )
+            }
+            </section>
+        );
+    }
+}
